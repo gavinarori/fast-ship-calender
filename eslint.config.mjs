24f943a0df4db31e00445ecf75becb65cc2cecbc
gavinarori <arorigavin@gmail.com>
2025-03-31 12:30:09 +0300

    updated the rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-var": 0,
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
